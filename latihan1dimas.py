fa2b0a73c79db263d6b9d5a01e4bf3d4f3382d62
dimas <dimas.dms15@gmail.com>
2019-10-27 17:48:38 +0700

    file pertama saya

---
+++ latihan1dimas.py
@@ -0,0 +1,16 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 20:34:20) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=input("masuukan nilai a:")
+masuukan nilai a:20
+>>> b=input("masukkan nilai b:")
+masukkan nilai b:2
+>>> print("hasil penggabungan {1}&{0}=%d" .format(20,2) %(20+2))
+hasil penggabungan 2&20=22
+>>> #konversi nilai variable
+>>> a=int(20)
+>>> b=int(2)
+>>> print("hasil penjumlahan {1}+{0}+%d" .format(20,2) %(20+2))
+hasil penjumlahan 2+20+22
+>>> print("hasil pembagian {1}/{0}=%d" .format(20,2) %(20/2))
+hasil pembagian 2/20=10
+>>> 
